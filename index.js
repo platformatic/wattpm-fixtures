07cedc4bfb2d869ae229c2370f504560845eb50a
Paolo Insogna <paolo@cowtech.it>
2024-11-14 14:28:25 +0100

    Update index.js
    Make the service bootable.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+const http = require("node:http")
+
+module.exports = function create () {
+  return http.createServer((_, res) => {
+    res.writeHead(200, { 'content-type': 'application/json', connection: 'close' })
+    res.end(JSON.stringify({ ok: true }))
+  })
+}
